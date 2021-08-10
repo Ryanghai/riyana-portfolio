090284c3f5b53277079e362ea44c098aaaa9c782
GitHub-Ryanghai <68055743+Ryanghai@users.noreply.github.com>
2021-08-10 09:08:03 +0900

    initial-commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -23,6 +22,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
+}
+
+body {
+  margin: 0;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,155 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="all-wrapper">
+    <div class="top-image">
+      <img src="../assets/top-image.png" alt="">
+    </div>
+    <div class="main-container">
+      <div class="main-icon">
+        <img src="../assets/icon.png" alt="">
+      </div>
+      <div class="main-text">
+        <p class="name">Riyana Harima</p>
+        <div class="introduce">
+          <p>イラスト・映画が好きな20歳<br />エンジニア歴約1年半</p>
+        </div>
+      </div>
+      <div class="main-sns">
+        <div class="sns-title">
+          <span>SNS</span>
+        </div>
+        <div class="sns-contents">
+          <a href="https://twitter.com/RyangHai" class="item">
+            <div class="sns-icon"><img src="../assets/twitter.svg" alt=""></div>
+          </a>
+          <a href="https://www.instagram.com/riyanar1t00/" class="item">
+            <div class="sns-icon"><img src="../assets/whiteIG.svg" alt=""></div>
+          </a>
+        </div>
+      </div>
+      <div class="main-portfolio">
+        <div class="portfolio-title">
+          <span>Portfolio</span>
+        </div>
+        <div class="portfolio-contents">
+          <a href="https://sugoi-mojiokoshi.varygoodkun.net" class="portfolio-item">
+            <div class="portfolio-icon"><p>すごい文字起こし<br>ばりぐっどくん</p></div>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.top-image {
+  position: relative;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.top-image img{
+  width: 100vw;
+  height: auto;
+  z-index: 2;
 }
-li {
+
+.main-container {
+  position: absolute;
+  z-index: 20;
+  width: 100vw;
+  margin-top: -40px;
+  height: calc(100vh - 175px);
+  background-color: #ffffff;
+  border-radius: 20px 20px 0 0;
+}
+
+.main-icon img {
+  width: 100px;
+  height: 100px;
+  margin-top: -60px;
+  border-radius: 50%;
+}
+
+.name {
+  margin: 0;
+  font-weight: bold;
+  color: #221f38;
+  font-size: 18px;
+}
+
+.introduce p {
+  margin: 0 auto;
+  font-size: 14px;
+  width: 70%;
+  color: #545261;
+}
+
+.main-sns {
+  margin-top: 20px;
+}
+
+.sns-title span {
+  font-weight: bold;
+  border-bottom: 1px solid #221f38;
+}
+
+.sns-contents {
+  overflow-x: auto;
+  white-space: nowrap;
+  -webkit-overflow-scrolling: touch;
+}
+
+.item {
   display: inline-block;
-  margin: 0 10px;
+  margin: 16px 8px;
+  background: #9893bd;
+  border-radius: 50%;
 }
-a {
-  color: #42b983;
+
+.sns-icon {
+  width: 100px;
+  height: 100px;
+}
+
+.sns-icon img {
+  width: 60px;
+  height: 60px;
+  margin: 22px 0 0 0;
+}
+
+.main-portfolio {
+  margin-top: 20px;
+}
+
+.portfolio-title span {
+  font-weight: bold;
+  border-bottom: 1px solid #221f38;
+}
+
+.portfolio-contents {
+  overflow-x: auto;
+  white-space: nowrap;
+  -webkit-overflow-scrolling: touch;
+}
+
+.portfolio-item {
+  display: inline-block;
+  margin: 16px 8px;
+  background: #9893bd;
+  text-decoration: none;
+  color: #fff;
+}
+
+.portfolio-icon {
+  width: 180px;
+  height: 120px;
+}
+
+.portfolio-icon p {
+  padding-top: 30px;
 }
 </style>
